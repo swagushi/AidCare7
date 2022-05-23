80bd9501b89ce480c4ef645a027b9f1ac5ddf8cb
Aayush Bhandari <ac111710@avcol.school.nz>
2022-05-23 15:04:49 +1200

    Capped the donation amount to 5,10,000 for tax evasion purposes

--- AidCare7/Migrations/20220523025821_Donation AMount Cap.Designer.cs
+++ AidCare7/Migrations/20220523025821_Donation AMount Cap.Designer.cs
@@ -4,14 +4,16 @@ using AidCare7.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AidCare7.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220523025821_Donation AMount Cap")]
+    partial class DonationAMountCap
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AidCare7/Migrations/20220523025821_Donation AMount Cap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AidCare7.Migrations
+{
+    public partial class DonationAMountCap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OUT_P_NO",
+                table: "donation",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OUT_P_NO",
+                table: "donation");
+        }
+    }
+}
